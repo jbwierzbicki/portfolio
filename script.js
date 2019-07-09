f8721d172e1df36e61cbd07a97c1b10f20d0b8ce
Johnathan <jbwierzbicki@gmail.com>
2019-07-08 21:06:33 -0400

    buncha changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 flexFont = function () {
     var divs = document.getElementsByClassName("flexFont");
     for(var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetWidth*0.05;
+        var relFontsize = divs[i].offsetWidth*0.1647;
         divs[i].style.fontSize = relFontsize+'px';
     }
 };
