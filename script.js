a04d6f477a2d17f5097a5a736b95f5d177156903
Johnathan <jbwierzbicki@gmail.com>
2019-07-09 13:19:04 -0400

    spacing adjust

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 flexFont = function () {
     var divs = document.getElementsByClassName("flexFont");
     for(var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetWidth*0.1647;
+        var relFontsize = divs[i].offsetWidth*0.17;
         divs[i].style.fontSize = relFontsize+'px';
     }
 };
